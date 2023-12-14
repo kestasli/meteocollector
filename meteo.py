5faaca114556e24f680b002aea8acfa98d48a7a3
kestasli <kestas.li@icloud.com>
2023-12-14 09:21:37 +0200

    VU meteo retrieval added

--- meteo.py
+++ meteo.py
@@ -18,23 +18,6 @@ def fmtMessage(topic, content):
    msg = {'topic': topic, 'payload': content, 'qos': 0, 'retain': False}
    return msg
 
-# setting callbacks for different events to see if it works, print the message etc.
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s." % rc)
-
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    #print("mid: " + str(mid))
-    None
-
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-# print message, useful for checking if it was successful
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
 def getRH(T, TD):
   RH = 100 * (exp((17.625 * TD)/(243.04 + TD))/exp((17.625 * T)/( 243.04 + T)))
   return RH
@@ -52,16 +35,14 @@ def convertDirection(direction):
       degrees = -1
    return degrees
 
-url = 'https://eismoinfo.lt/weather-conditions-service'
+url_eismoinfo = 'https://eismoinfo.lt/weather-conditions-service'
 
-session = request.urlopen(url)
+session = request.urlopen(url_eismoinfo)
 data = str(session.read().decode(encoding='UTF-8'))
 # print(session.info())
 session.close()
 
 js_data_kd = json.loads(data)
-print('\nEismoinfo.lt:\n')
-
 stationsList = []
 
 for i in js_data_kd:
@@ -71,23 +52,27 @@ for i in js_data_kd:
     with open('stationdata/' + i['id'] + '.json', "w") as outfile:
         outfile.write(stationData)
 
-publish.multiple(stationsList, hostname = mqtt_server, port = 8883, auth=user_pass, tls={'ca_certs': 'root-CA.crt'})
-
-print(len(stationsList))
-
-'''
-url = 'http://www.hkk.gf.vu.lt/json.php'
+################################################################################
+url_vu = 'http://www.hkk.gf.vu.lt/ms_json.php'
 
 try:
-    session = request.urlopen(url)
+    req = request.Request(url_vu)
+    req.add_header('Referer', 'https://www.hkk.gf.vu.lt/vu_ms/')
+    session = request.urlopen(req)
     #print(session.info())
     data = str(session.read().decode(encoding='UTF-8'))
     session.close()
-    data = data[4:-4]
+    data = data[4:-3]
     js_data_vu = json.loads(data)
-    print('\nVU meteo:\n')
-    print('Vilnius: %s C, %s m/s, %s %%' % (js_data_vu['zeno_AT_5s_C'],
-    js_data_vu['zeno_Spd_5s_Kt'], js_data_vu['zeno_RH_5s']))
+    print(js_data_vu['zeno_AT_5s_C'], js_data_vu['zeno_Dir_5s'])
+    stationData = formatMQData(js_data_vu['zeno_AT_5s_C'], js_data_vu['zeno_Spd_5s_Kt'], js_data_vu['zeno_Dir_5s'], '0', 'VU Meteo Stotis')
+    stationsList.append(fmtMessage(root_topic + '/' + '0', stationData))
+    print(stationData)
+
 except error.URLError as err:
     print(err)
-'''
+
+publish.multiple(stationsList, hostname = mqtt_server, port = 8883, auth=user_pass, tls={'ca_certs': 'root-CA.crt'})
+
+pp.pprint(stationsList)
+print(len(stationsList))
